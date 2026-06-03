7be3784f37c1caafc812c96a90e5843e214acd5d
Luiz Carlos Reis <contato@rmasolutions.com.br>
2026-06-03 16:54:05 -0300

    feat: remover layout B e deixar ativo a versao A (preta)

--- src/app/(default)/layout.tsx
+++ src/app/(default)/layout.tsx
@@ -1,6 +1,5 @@
 import Navbar from "@/components/navbar";
 import Footer from "@/components/footer";
-import LayoutSwitcher from "@/components/layout-switcher";
 
 export default function DefaultLayout({
   children,
@@ -12,7 +11,6 @@ export default function DefaultLayout({
       <Navbar />
       <main className="flex-grow">{children}</main>
       <Footer />
-      <LayoutSwitcher />
     </>
   );
 }
